2efceaf2812b365d2dc4e0e2412234c6c85287bf
KennieJ <kennie.jfive@gmail.com>
2020-11-17 19:13:21 -0500

    initial commit

---
+++ UnitTestProject1/LoopExamples.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _03_Loops
+{
+    [TestClass]
+    public class LoopExamples
+    {
+        [TestMethod]
+        public void ForLoops()
+        {
+            int studentCount = 15;
+            //1 Starting point; fires only once
+            //2 Condition that keeps the loop running while true
+            //3 What happens after each loop
+            //4 Body of the loop. What happens each interation
+
+            //for   //1             //2       //3
+            for (int i = 0; i < studentCount; i++) //"i++" is same as "i = i + 1"
+            {
+                //4
+                Console.WriteLine(i);
+            }
+        }
+
+        [TestMethod]
+        public void ForeachLoops()
+        {
+            string name = "Eleven Fifty Academy";
+
+            //1 Collection thats being worked on
+            //2 Name of the current iteration being used
+            //3 Current type in the collection
+            //4 in keyword, used to separate the individual and the collection
+            //5 body of the loop
+            //foreach //3  //2  //4  //1
+            foreach (char letter in name)
+            {
+                Console.WriteLine(letter); //5
+            }
+        }
+
+        [TestMethod]
+        public void WhileLoops()
+        {
+            int total = 1;
+
+            //1 Loop runs while the condition is true
+            //2 The body of the loop
+
+            //while //1
+            while (total != 30)
+            {
+                Console.WriteLine(total); //2
+                total++; //total = total +1
+            }
+        }
+    }
+}
---
+++ UnitTestProject1/ThursChal.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _01_ThursdayChallenge
+{
+    [TestClass]
+    public class ThursChal
+    {
+        [TestMethod]
+        public void SuperCal()
+        {
+            string longWord = "Supercalifragilisticexpialidocious";
+            int letterCount = longWord.Length;
+
+            foreach (char letter in longWord)
+            {
+                if (letter == 'i')
+                {
+                    Console.WriteLine(letter);
+                }
+                else if (letter == 'l')
+                {
+                    Console.WriteLine(letter);
+                }
+                else
+                {
+                    Console.WriteLine("Not an i or an L");
+                }
+                
+            }
+
+            Console.WriteLine(letterCount);
+        }
+
+        [TestMethod]
+        public void Variables()
+        {
+            string firstName = "Kendra";
+            string lastName = "Joseph";
+            int myAge = 32;
+
+            string[] holidayMovies = { "Nightmare Before Christmas", "A Muppet Christmas Carol", "How the Grinch Stole Christmas", "8 Crazy Nights" };
+            DateTime now = DateTime.Now;
+            DateTime birthDate = new DateTime(1988, 9, 16);
+            List<DateTime> listOfDates = new List<DateTime>();
+            listOfDates.Add(now);
+            listOfDates.Add(birthDate);
+            
+            foreach (DateTime date in listOfDates)
+            {
+                Console.WriteLine(date);
+            }
+        }
+
+        [TestMethod]
+        public void Operators()
+        {
+            int myAge = 32;
+
+            int sum = myAge + 7;
+            int diff = myAge - 7;
+            int prod = myAge * 7;
+            int quot = myAge / 7;
+            int remain = myAge % 7;
+
+            Console.WriteLine(sum);
+            Console.WriteLine(diff);
+            Console.WriteLine(prod);
+            Console.WriteLine(quot);
+            Console.WriteLine(remain);
+        }
+
+        [TestMethod]
+        public void Sleep()
+        {
+            Console.WriteLine("How many hours of sleep did you get?");
+            int sleepHours = 2;
+
+            if (sleepHours >= 10)
+            {
+                Console.WriteLine("Wow that's a lot of sleep!");
+            }
+            else if (sleepHours >= 8)
+            {
+                Console.WriteLine("You should be pretty rested.");
+            }
+            else if (sleepHours >= 4)
+            {
+                Console.WriteLine("Bummer");
+            }
+            else
+            {
+                Console.WriteLine("Oh man, get some sleep!");
+            }
+        }
+
+        [TestMethod]
+        public void HowYouDoin()
+        {
+            string mood = ":("; //assign an int to inputs to reduce user
+            switch (mood)
+            {
+                case "Great":
+                    Console.WriteLine("That's fantastic!");
+                    break;
+                case "Good":
+                    Console.WriteLine("Glad to hear that");
+                    break;
+                case "Okay":
+                    Console.WriteLine("I feel you");
+                    break;
+                case "Bad":
+                    Console.WriteLine("Oh no! What happened?");
+                    break;
+                case ":(":
+                    Console.WriteLine(":c");
+                    break;
+                default:
+                    Console.WriteLine("I don't understand. Try typing 'Great', 'Good', 'Okay', 'Bad', or ':('");
+                    break;
+            }
+        }
+    }
+}
