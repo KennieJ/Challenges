eb55226153723f9554b2151dc4135c44c386f56a
KennieJ <kennie.jfive@gmail.com>
2020-12-07 23:27:23 -0500

    add week 4 challenges

---
+++ Week04Challenges/InheritanceClasses.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week04Challenges
+{
+    abstract class ChemicalInventory
+    {
+        public string FullName { get; set; }
+        public string CommonName { get; set; }
+        public string Vendor { get; set; }
+        public string LotNumber { get; set; }
+        public bool InStock { get; set; }
+        public string StorageLocation { get; set; }
+
+        public ChemicalInventory() { }
+
+        public ChemicalInventory(string fullName, string commonName, string vendor, string lotNumber, bool inStock, string storageLocation)
+        {
+            FullName = fullName;
+            CommonName = commonName;
+            Vendor = vendor;
+            LotNumber = lotNumber;
+            InStock = inStock;
+            StorageLocation = storageLocation;
+        }
+    }
+
+    class ReferenceStandards : ChemicalInventory
+    {
+        public bool IsDeuterated { get; set; }
+        public string DrugClass { get; set; }
+
+        public ReferenceStandards() { }
+        public ReferenceStandards(string fullName, string commonName, string vendor, string lotNumber, bool inStock, string storageLocation, bool isDeuterated, string drugClass)
+            :base(fullName, commonName, vendor, lotNumber, inStock, storageLocation)
+        {
+            IsDeuterated = isDeuterated;
+            DrugClass = drugClass;
+        }
+    }
+
+    class Reagents : ChemicalInventory
+    {
+        public bool UseInFumeHood { get; set; }
+        public double LitersRemaining { get; set; }
+
+        public Reagents() { }
+        public Reagents(string fullName, string commonName, string vendor, string lotNumber, bool inStock, string storageLocation, bool useInFumeHood, double litersRemaining)
+            :base(fullName, commonName, vendor, lotNumber, inStock, storageLocation)
+        {
+            UseInFumeHood = useInFumeHood;
+            LitersRemaining = litersRemaining;
+        }
+    }
+
+    class StockSolutions : ReferenceStandards
+    {
+        public bool PassedLotTest { get; set; }
+        public DateTime PrepDate { get; set; }
+
+        public StockSolutions() { }
+
+        public StockSolutions(string fullName, string commonName, string vendor, string lotNumber, bool inStock, string storageLocation, bool isDeuterated,
+            string drugClass, bool passedLotTest, DateTime prepDate)
+            :base (fullName, commonName, vendor, lotNumber, inStock, storageLocation, isDeuterated, drugClass)
+        {
+            PassedLotTest = passedLotTest;
+            PrepDate = prepDate;
+        }
+        
+    }
+}
---
+++ Week04Challenges/Main.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Week04Challenges
+{
+    [TestClass]
+    public class Main
+    {
+        [TestMethod]
+        public void InventoryTest()
+        {
+            var alpraz = new ReferenceStandards("Alprazolam", "Xanax", "Cerilliant", "FN195264802", true, "Freezer 1", false, "Benzodiazepines");
+            var opiStock1 = new StockSolutions("Opioid Method Stock Solution", "OpiSS", "In-house", "20201206-OSS", true, "Freezer 2", false, "Opioids", true, DateTime.Today);
+
+            var ACN = new Reagents();
+            ACN.FullName = "Acetonitrile";
+            ACN.InStock = true;
+            ACN.LitersRemaining = 5.5;
+            ACN.UseInFumeHood = true;
+            ACN.StorageLocation = "Flammables Cabinet";
+        }
+    }
+}
